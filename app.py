d4a91c72a81cbc02311e856a024917f31e514656
Siddhartha Nevrear <sid.nevrekar@proton.me>
2025-08-27 11:56:32 -0500

    added original score

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ st.header("Made By @Sid")
 st.title("SQRT Curve Calculator")
 
 score_input = st.text_input("Enter your score and total (e.g., 8/10)")
-
 if score_input:
     try:
         score1, score2 = map(int, score_input.split("/"))
+        rawscore = score1 / score2 * 100
         percantagescore = (math.sqrt(score1) / math.sqrt(score2)) * 100
-        st.success(f"Curved Score: {percantagescore:.2f}%")
+        st.success(f" Inital Score: {rawscore:.2f}% | Curved Score: {percantagescore:.2f}%")
     except:
         st.error("Invalid input. Use format 8/10.")
+
