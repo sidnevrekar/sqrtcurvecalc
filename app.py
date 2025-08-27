2709f602108e4352e1042d2ecb618a2262b1ab59
Siddhartha Nevrekar <siddhartha.nevrekar@gmail.com>
2025-08-27 09:46:37 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import math
+import streamlit as st
+st.header("Made By @Sid")
+st.title("SQRT Curve Calculator")
+
+score_input = st.text_input("Enter your score and total (e.g., 8/10)")
+
+if score_input:
+    try:
+        score1, score2 = map(int, score_input.split("/"))
+        percantagescore = (math.sqrt(score1) / math.sqrt(score2)) * 100
+        st.success(f"Curved Score: {percantagescore:.2f}%")
+    except:
+        st.error("Invalid input. Use format 8/10.")
